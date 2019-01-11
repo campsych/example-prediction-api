41c904619eeea4fa701b82324b55adad0eb4a5ad
Bartosz Kielczewski <bartosz@kielczewski.eu>
2019-01-11 17:11:26 +0000

    Removed unused param from /text call

--- python/example.py
+++ python/example.py
@@ -18,9 +18,6 @@ def auth(customer_id, api_key):
 def predict_from_text(token, text):
     try:
         response = requests.post(url='https://api.applymagicsauce.com/text',
-                                 params={
-                                     'source': 'OTHER'
-                                 },
                                  data=text,
                                  headers={'X-Auth-Token': token})
         response.raise_for_status()
