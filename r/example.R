17b278a3f5e6690291366f61d56942d86c44194f
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-06-13 13:36:09 +0100

    Improved style

--- r/example.R
+++ r/example.R
@@ -56,21 +56,25 @@ PredictFromLikeNames <- function(token, likeNames) {
     }
 }
 
-# /auth
-token <- Auth(1234, 'key')
+Predict <- function(customerId, apiKey) {
+    # /auth
+    token <- Auth(customerId, apiKey)
 
-# /text
-predictionResult <- PredictFromText(token, "Lorem ipsum dolor sit amet")
-print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
+    # /text
+    predictionResult <- PredictFromText(token, "Lorem ipsum dolor sit amet")
+    print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
 
-# /like ids
-predictionResult <- PredictFromLikeIds(token, c("5845317146", "6460713406", "22404294985", "35312278675",
-                                                "105930651606", "171605907303", "199592894970", "274598553922",
-                                                "340368556015", "100270610030980"))
-print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
+    # /like ids
+    predictionResult <- PredictFromLikeIds(token, c("5845317146", "6460713406", "22404294985", "35312278675",
+                                                    "105930651606", "171605907303", "199592894970", "274598553922",
+                                                    "340368556015", "100270610030980"))
+    print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
 
-# /like names
-# Populate the array below with proper names (i.e. name of a celebrity, band, organisation, etc.), something well-known.
-# Come up with 10 of these.
-predictionResult <- PredictFromLikeNames(token, c("Name 1", "Name 2", "Name 3"))
-print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
+    # /like names
+    # Populate the array below with proper names (i.e. name of a celebrity, band, organisation, etc.), something well-known.
+    # Come up with 10 of these.
+    predictionResult <- PredictFromLikeNames(token, c("Name 1", "Name 2", "Name 3"))
+    print(jsonlite::toJSON(predictionResult, pretty = TRUE, auto_unbox = TRUE))
+}
+
+Predict(1234, 'key')
